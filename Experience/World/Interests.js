6026fefa31f8c55adc2218f21679d7af2b8fe5c7
Yin jieyang <103308766+SeeYouCowboi@users.noreply.github.com>
2024-04-07 02:23:39 +0000

    修改logo，让所有小部件都加入点击显示信息

--- Experience/World/Interests.js
+++ Experience/World/Interests.js
@@ -158,8 +158,8 @@ export default class Interests {
     const nabi = document.querySelector(".nabi");
     const lumen = document.querySelector(".lumen");
 
-    const pharmacyComputer = document.querySelector(".pharmacy-computer");
     const pharmacyShelf = document.querySelector(".pharmacy-shelf");
+    const pharmacyComputer = document.querySelector(".pharmacy-computer");
     const vexToolkit = document.querySelector(".vex-toolkit");
     const vexComputer = document.querySelector(".vex-computer");
     const frontDesk = document.querySelector(".front-desk");
@@ -175,20 +175,21 @@ export default class Interests {
     const infoPanelDescription = document.querySelector(
       ".info-panel-description",
     );
-    // const infoPanelMo = document.querySelector(".info-panel-monday");
-    // const infoPanelTu = document.querySelector(".info-panel-tuesday");
-    // const infoPanelWe = document.querySelector(".info-panel-wednesday");
-    // const infoPanelTh = document.querySelector(".info-panel-thursday");
-    // const infoPanelFr = document.querySelector(".info-panel-friday");
-    // const infoPanelSa = document.querySelector(".info-panel-saturday");
-    // const infoPanelSu = document.querySelector(".info-panel-sunday");
+
     const infoPanelPhone = document.querySelector(".info-panel-phone");
     const infoPanelEmail = document.querySelector(".info-panel-email");
     // const infoPanelWebsite = document.querySelector(".info-panel-website");
     let infoPanelRightStyle = "0";
 
+    // 点开小部件后显示消息的数组
+    // index与部件对应：
+    // 0 -- frontComputer
+    // 1 -- frontDesk
+    // 2 -- vexComputer
+    // 3 -- vexToolkit
+    // 4 -- pharmacyComputer
+    // 5 -- pharmacyShelf
     const infos = [
-      // Museums
       {
         image: "/images/img-mcba.jpg",
         logo: "/images/logo-mcba.svg",
@@ -208,101 +209,101 @@ export default class Interests {
         contact: ["+41 21 316 34 45", "mcba@plateforme10.ch"],
         website: "https://www.mcba.ch/en/",
       },
-      // {
-      //   image: "/images/img-mudac.jpg",
-      //   logo: "/images/logo-mudac.svg",
-      //   title: "Musée Cantonal de Design et d’Arts Appliqués Contemporains",
-      //   lead: "mudac – Lausanne’s Museum of Contemporary Design and Applied Arts – is the only institution in Western Switzerland entirely dedicated to design.",
-      //   description: `It has developed its identity and international reputation through hundreds of ambitious and often unusual exhibitions and continues to pursue a policy of openness and exchange between the many disciplines of contemporary creation. <br><br>
-      //   Its programme showcases designers and artists at solo exhibitions and extends to exhibitions that question the public on contemporary social issues. It demonstrates the museum’s interest in the world and in the wide scope that the term design itself can encompass. <br><br>
-      //   The diversity of points of view of each project has enabled mudac to assert itself both on the national and international scene by offering its exhibitions to its counterparts. Europe and Asia have thus been able to discover exhibitions made in mudac on numerous occasions.`,
-      //   schedule: [
-      //     "10:00 - 18:00",
-      //     "Closed",
-      //     "10:00 - 18:00",
-      //     "10:00 - 20:00",
-      //     "10:00 - 18:00",
-      //     "10:00 - 18:00",
-      //     "10:00 - 18:00",
-      //   ],
-      //   contact: ["+41 21 318 44 00", "mudac@plateforme10.ch"],
-      //   website: "https://mudac.ch/en/",
-      // },
-      // {
-      //   image: "/images/img-elysee.jpg",
-      //   logo: "/images/logo-elysee.svg",
-      //   title: "Photo Elysée",
-      //   lead: "Photo Elysée is one of the most important museums entirely dedicated to the photographic medium. Each year, we produce demanding exhibitions, distribute reference editorial content, conceive innovative events and offer events open to all.",
-      //   description: `Since its creation in 1985 as a “museum for photography”, Photo Elysée has been questioning the permanent reinvention of the medium through the great figures who have marked its history by imagining new ways of seeing or making people see, while revealing in a privileged way the emerging photography which, through unseen views, bears witness to the world of today and prefigures that of tomorrow. In other words: to cover all the subjects of photography, to discover them sometimes, and above all to make them rediscovered. <br><br>
-      //   The museum’s collection of more than 1,200,000 phototypes covers the entire field of photography, from the first processes dating from the 1840s to the digital image. It includes many complete photographic collections or archives, including those of Sabine Weiss, Jan Groover, René Burri, Ella Maillart, Nicolas Bouvier, Charlie Chaplin, Gertrude Fehr, Hans Steiner and Olivier Föllmi.`,
-      //   schedule: [
-      //     "10:00 - 18:00",
-      //     "Closed",
-      //     "10:00 - 18:00",
-      //     "10:00 - 20:00",
-      //     "10:00 - 18:00",
-      //     "10:00 - 18:00",
-      //     "10:00 - 18:00",
-      //   ],
-      //   contact: ["+41 21 318 44 00", "info@elysee.ch"],
-      //   website: "https://elysee.ch/en",
-      // },
-      // // Restaurants & Cafés
-      // {
-      //   image: "/images/img-arcadia.jpg",
-      //   logo: "/images/logo-arcadia.svg",
-      //   title: "Arcadia Restaurant",
-      //   lead: `Authentic Mediterranean dining at the heart of Lausanne's arts district`,
-      //   description: `Arcadia Restaurant, with its seven iconic arches, opens the door to a la carte Mediterranean cuisine, balancing fresh flavours with seasonal local ingredients and products. Our menu is served alongside an inspired cocktail offering and a selection of local beer and wines. <br><br>
-      //   For those looking for a relaxed, tasty, and inclusive dining experience, whether at lunch for everyday cooking or in the evening for a more special occasion. Always in a relaxed and friendly environment.`,
-      //   schedule: [
-      //     "10:00 - 23:00",
-      //     "Closed",
-      //     "10:00 - 23:00",
-      //     "10:00 - 23:00",
-      //     "10:00 - 23:00",
-      //     "10:00 - 23:00",
-      //     "10:00 - 18:00",
-      //   ],
-      //   contact: ["+41 21 318 44 10", "info@arcadiarestaurant.ch"],
-      //   website: "https://www.arcadiarestaurant.ch/",
-      // },
-      // {
-      //   image: "/images/img-nabi.jpg",
-      //   logo: "/images/logo-nabi.svg",
-      //   title: "Le Nabi Café-Restaurant",
-      //   lead: `Traditional cuisine to savour before or after a visit to Plateforme 10!`,
-      //   description: `The menu aims to be inviting, familial, yet inventive. Traditional cooking is restyled while the menu follows the seasons by adapting to the harvest of local producers. The drinks are artisanal. Meticulous service at reasonable prices for a convivial moment that everyone can afford to enjoy. Kids are very much welcome, too. Menu selections are designed for savory discoveries, and a play area encourages creativity.`,
-      //   schedule: [
-      //     "Closed",
-      //     "09:30 - 18:00",
-      //     "09:30 - 18:00",
-      //     "09:30 - 20:00",
-      //     "09:30 - 18:00",
-      //     "09:30 - 18:00",
-      //     "09:30 - 18:00",
-      //   ],
-      //   contact: ["+41 21 311 02 90", "info@lenabi.ch"],
-      //   website: "https://www.mcba.ch/en/cafe-restaurant-2/",
-      // },
-      // {
-      //   image: "/images/img-lumen.jpg",
-      //   logo: "/images/logo-lumen.svg",
-      //   title: "Lumen Café",
-      //   lead: `A spontaneous and fast cuisine with authentic artisanal products`,
-      //   description: `Le Café Lumen invites you to discover its menu of quick and spontaneous dishes that use artisanal products served simply on a board, in a clay bowl, or nestled between two slices of focaccia. Here you will find the best of the region’s culinary tradition. All our drinks are artisanal with a fine selection of craft cider and beer, wine, and homemade non-alcoholic beverages. Our coffee is freshly roasted. We’re a stop along the region’s greenway, the Voie Verte foot and bicycle path, with takeaway service available.`,
-      //   schedule: [
-      //     "10:00 - 18:00",
-      //     "Closed",
-      //     "10:00 - 18:00",
-      //     "10:00 - 20:00",
-      //     "10:00 - 18:00",
-      //     "10:00 - 18:00",
-      //     "10:00 - 18:00",
-      //   ],
-      //   contact: ["+41 21 311 02 90", "info@cafelumen.ch"],
-      //   website: "https://mudac.ch/de/cafe-lumen/",
-      // },
+      {
+        image: "/images/img-mcba.jpg",
+        logo: "/images/logo-mcba.svg",
+        title: "Musée Cantonal des Beaux-Arts Lausanne",
+        lead: "Opened in 1841, the Vaud Museum of Fine Arts in Lausanne is one of the oldest Swiss museums exclusively dedicated to art. Now located in Plateforme 10, it presents several temporary exhibitions a year from its collection of 10,000 works.",
+        description: `Ducros, Gleyre, Steinlen, Vallotton and Soutter: these are some of the Vaudois painters for which the Vaud Museum of Fine Arts (MCBA) is known, nationally and internationally. Their works form a large part of the museum’s collection, currently comprising some 10,000 paintings. <br><br>
+        The MCBA organises several temporary exhibitions every year, each one accompanied with a programme of cultural activities including events, guided tours and workshops for adults and children. Admission is free on the first Saturday of the month.`,
+        schedule: [
+          "Closed",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 20:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+        ],
+        contact: ["+41 21 316 34 45", "mcba@plateforme10.ch"],
+        website: "https://www.mcba.ch/en/",
+      },
+      {
+        image: "/images/img-mcba.jpg",
+        logo: "/images/logo-mcba.svg",
+        title: "Musée Cantonal des Beaux-Arts Lausanne",
+        lead: "Opened in 1841, the Vaud Museum of Fine Arts in Lausanne is one of the oldest Swiss museums exclusively dedicated to art. Now located in Plateforme 10, it presents several temporary exhibitions a year from its collection of 10,000 works.",
+        description: `Ducros, Gleyre, Steinlen, Vallotton and Soutter: these are some of the Vaudois painters for which the Vaud Museum of Fine Arts (MCBA) is known, nationally and internationally. Their works form a large part of the museum’s collection, currently comprising some 10,000 paintings. <br><br>
+        The MCBA organises several temporary exhibitions every year, each one accompanied with a programme of cultural activities including events, guided tours and workshops for adults and children. Admission is free on the first Saturday of the month.`,
+        schedule: [
+          "Closed",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 20:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+        ],
+        contact: ["+41 21 316 34 45", "mcba@plateforme10.ch"],
+        website: "https://www.mcba.ch/en/",
+      },
+      {
+        image: "/images/img-mcba.jpg",
+        logo: "/images/logo-mcba.svg",
+        title: "Musée Cantonal des Beaux-Arts Lausanne",
+        lead: "Opened in 1841, the Vaud Museum of Fine Arts in Lausanne is one of the oldest Swiss museums exclusively dedicated to art. Now located in Plateforme 10, it presents several temporary exhibitions a year from its collection of 10,000 works.",
+        description: `Ducros, Gleyre, Steinlen, Vallotton and Soutter: these are some of the Vaudois painters for which the Vaud Museum of Fine Arts (MCBA) is known, nationally and internationally. Their works form a large part of the museum’s collection, currently comprising some 10,000 paintings. <br><br>
+        The MCBA organises several temporary exhibitions every year, each one accompanied with a programme of cultural activities including events, guided tours and workshops for adults and children. Admission is free on the first Saturday of the month.`,
+        schedule: [
+          "Closed",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 20:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+        ],
+        contact: ["+41 21 316 34 45", "mcba@plateforme10.ch"],
+        website: "https://www.mcba.ch/en/",
+      },
+      {
+        image: "/images/img-mcba.jpg",
+        logo: "/images/logo-mcba.svg",
+        title: "Musée Cantonal des Beaux-Arts Lausanne",
+        lead: "Opened in 1841, the Vaud Museum of Fine Arts in Lausanne is one of the oldest Swiss museums exclusively dedicated to art. Now located in Plateforme 10, it presents several temporary exhibitions a year from its collection of 10,000 works.",
+        description: `Ducros, Gleyre, Steinlen, Vallotton and Soutter: these are some of the Vaudois painters for which the Vaud Museum of Fine Arts (MCBA) is known, nationally and internationally. Their works form a large part of the museum’s collection, currently comprising some 10,000 paintings. <br><br>
+        The MCBA organises several temporary exhibitions every year, each one accompanied with a programme of cultural activities including events, guided tours and workshops for adults and children. Admission is free on the first Saturday of the month.`,
+        schedule: [
+          "Closed",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 20:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+        ],
+        contact: ["+41 21 316 34 45", "mcba@plateforme10.ch"],
+        website: "https://www.mcba.ch/en/",
+      },
+      {
+        image: "/images/img-mcba.jpg",
+        logo: "/images/logo-mcba.svg",
+        title: "Musée Cantonal des Beaux-Arts Lausanne",
+        lead: "Opened in 1841, the Vaud Museum of Fine Arts in Lausanne is one of the oldest Swiss museums exclusively dedicated to art. Now located in Plateforme 10, it presents several temporary exhibitions a year from its collection of 10,000 works.",
+        description: `Ducros, Gleyre, Steinlen, Vallotton and Soutter: these are some of the Vaudois painters for which the Vaud Museum of Fine Arts (MCBA) is known, nationally and internationally. Their works form a large part of the museum’s collection, currently comprising some 10,000 paintings. <br><br>
+        The MCBA organises several temporary exhibitions every year, each one accompanied with a programme of cultural activities including events, guided tours and workshops for adults and children. Admission is free on the first Saturday of the month.`,
+        schedule: [
+          "Closed",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 20:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+          "10:00 - 18:00",
+        ],
+        contact: ["+41 21 316 34 45", "mcba@plateforme10.ch"],
+        website: "https://www.mcba.ch/en/",
+      },
     ];
 
     if (this.device === "desktop") {
@@ -454,7 +455,7 @@ export default class Interests {
       });
     });
 
-    frontDesk.addEventListener("click", () => {
+    frontComputer.addEventListener("click", () => {
       this.scrolling.target = 0;
       infoPanel.style.right = "0";
       const infoIndex = 0;
@@ -465,24 +466,59 @@ export default class Interests {
       infoPanelEmail.innerHTML = infos[infoIndex].contact[1];
     });
 
-    nabi.addEventListener("click", () => {
+    frontDesk.addEventListener("click", () => {
       this.scrolling.target = 0;
       infoPanel.style.right = "0";
-      infoPanelImage.src = infos[4].image;
-      infoPanelLogo.src = infos[4].logo;
-      infoPanelTitle.innerHTML = infos[4].title;
-      infoPanelLead.innerHTML = infos[4].lead;
-      infoPanelDescription.innerHTML = infos[4].description;
-      infoPanelMo.innerHTML = infos[4].schedule[0];
-      infoPanelTu.innerHTML = infos[4].schedule[1];
-      infoPanelWe.innerHTML = infos[4].schedule[2];
-      infoPanelTh.innerHTML = infos[4].schedule[3];
-      infoPanelFr.innerHTML = infos[4].schedule[4];
-      infoPanelSa.innerHTML = infos[4].schedule[5];
-      infoPanelSu.innerHTML = infos[4].schedule[6];
-      infoPanelPhone.innerHTML = infos[4].contact[0];
-      infoPanelEmail.innerHTML = infos[4].contact[1];
-      infoPanelWebsite.href = infos[4].website;
+      const infoIndex = 1;
+      infoPanelImage.src = infos[infoIndex].image;
+      infoPanelTitle.innerHTML = infos[infoIndex].title;
+      infoPanelDescription.innerHTML = infos[infoIndex].description;
+      infoPanelPhone.innerHTML = infos[infoIndex].contact[0];
+      infoPanelEmail.innerHTML = infos[infoIndex].contact[1];
+    });
+
+    vexComputer.addEventListener("click", () => {
+      this.scrolling.target = 0;
+      infoPanel.style.right = "0";
+      const infoIndex = 2;
+      infoPanelImage.src = infos[infoIndex].image;
+      infoPanelTitle.innerHTML = infos[infoIndex].title;
+      infoPanelDescription.innerHTML = infos[infoIndex].description;
+      infoPanelPhone.innerHTML = infos[infoIndex].contact[0];
+      infoPanelEmail.innerHTML = infos[infoIndex].contact[1];
+    });
+
+    vexToolkit.addEventListener("click", () => {
+      this.scrolling.target = 0;
+      infoPanel.style.right = "0";
+      const infoIndex = 3;
+      infoPanelImage.src = infos[infoIndex].image;
+      infoPanelTitle.innerHTML = infos[infoIndex].title;
+      infoPanelDescription.innerHTML = infos[infoIndex].description;
+      infoPanelPhone.innerHTML = infos[infoIndex].contact[0];
+      infoPanelEmail.innerHTML = infos[infoIndex].contact[1];
+    });
+
+    pharmacyComputer.addEventListener("click", () => {
+      this.scrolling.target = 0;
+      infoPanel.style.right = "0";
+      const infoIndex = 4;
+      infoPanelImage.src = infos[infoIndex].image;
+      infoPanelTitle.innerHTML = infos[infoIndex].title;
+      infoPanelDescription.innerHTML = infos[infoIndex].description;
+      infoPanelPhone.innerHTML = infos[infoIndex].contact[0];
+      infoPanelEmail.innerHTML = infos[infoIndex].contact[1];
+    });
+
+    pharmacyShelf.addEventListener("click", () => {
+      this.scrolling.target = 0;
+      infoPanel.style.right = "0";
+      const infoIndex = 5;
+      infoPanelImage.src = infos[infoIndex].image;
+      infoPanelTitle.innerHTML = infos[infoIndex].title;
+      infoPanelDescription.innerHTML = infos[infoIndex].description;
+      infoPanelPhone.innerHTML = infos[infoIndex].contact[0];
+      infoPanelEmail.innerHTML = infos[infoIndex].contact[1];
     });
 
     lumen.addEventListener("click", () => {
